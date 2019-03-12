bfe125d593ac2d5944f59ffc1243d5120c71a7bd
Diana Martinez Aguilera <diana.martinez@vasslatam.com>
2019-03-12 10:03:57 -0500

    cambiorutas2

--- lb_update.sh
+++ lb_update.sh
@@ -1,2 +1,1 @@
-./liquibase 
---changeLogFile=/var/lib/jenkins/workspace/Pruebaliquidbase/update.xml update
+./liquibase --changeLogFile=/var/lib/jenkins/workspace/Pruebaliquidbase/update.xml update
