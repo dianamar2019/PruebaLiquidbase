c945f39a03f6db252648bb4f923d53746e44981b
Diana Martinez Aguilera <diana.martinez@vasslatam.com>
2019-03-19 14:09:50 -0500

    eliminarprocedure6

--- DespliegueInicial/Rollback/Eliminarprocedure.sql
+++ DespliegueInicial/Rollback/Eliminarprocedure.sql
@@ -1,3 +1,10 @@
-drop procedure update_usuarios;
+begin
+   execute immediate 'drop procedure update_usuarios';
+exception when others then
+   if sqlcode != -4043 then
+      raise;
+   end if;
+end;
+/
 
 
