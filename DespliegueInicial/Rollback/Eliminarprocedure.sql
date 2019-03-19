38d50fca90c1b9eb9f6bc6adbc08e7ae65a54c88
Diana Martinez Aguilera <diana.martinez@vasslatam.com>
2019-03-19 15:30:33 -0500

    eliminarprocedure7

--- DespliegueInicial/Rollback/Eliminarprocedure.sql
+++ DespliegueInicial/Rollback/Eliminarprocedure.sql
@@ -1,10 +1,17 @@
-begin
-   execute immediate 'drop procedure update_usuarios';
-exception when others then
-   if sqlcode != -4043 then
-      raise;
-   end if;
-end;
-/
+DECLARE
+  V_NUM NUMBER;
+BEGIN     
+  SELECT COUNT(*)
+  INTO   V_NUM
+  FROM   USER_OBJECTS
+  WHERE  OBJECT_NAME = 'update_usuarios'
+  AND    OBJECT_TYPE = 'PROCEDURE'; 
 
+  IF V_NUM > 0 THEN
+    EXECUTE IMMEDIATE 'drop procedure update_usuarios';
+    DBMS_OUTPUT.PUT_LINE('Dropped');
+  END IF;
+
+END;
+/
 
